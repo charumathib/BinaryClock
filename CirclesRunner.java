7414a9e3e3faa35cf3d1a66c041321ac5bb2eed4
Charumathi Badrinath <charumathi_badrinath@ryecountryday.org>
2017-11-18 13:31:54 -0500

    Changed if-statements to for-loops

--- CirclesRunner.java
+++ CirclesRunner.java
@@ -29,53 +29,38 @@ public class CirclesRunner extends JPanel{
         String binaryHours = convertToBinary(h);
         String binaryMinutes = convertToBinary(m);
         String binarySeconds = convertToBinary(s);
-        System.out.println(binarySeconds);
-
+        
         //if the circle contains a 1 at a certain index, make the circle there black. Otherwise, leave it cyan
 
         int xPos = 500/5;
-        for(Circle h : hours){
-            h.paint(g, xPos, (500/4), Color.black);
-            xPos += 50;
-        }
-
-        xPos = 500/5;
-        for(Circle m : mins){
-            m.paint(g, xPos, (500/4) * 2, Color.black);
+        for(int i = 0; i < binarySeconds.length(); i++){
+            if(binaryHours.substring(i, i+1).equals("1")){
+                hours[i].paint(g, xPos, 500/4 , Color.cyan);
+            }else{
+                hours[i].paint(g, xPos, 500/4, Color.black);
+            }
             xPos += 50;
         }
         xPos = 500/5;
+        for(int i = 0; i < binarySeconds.length(); i++){
+            if(binaryMinutes.substring(i, i+1).equals("1")){
+                mins[i].paint(g, xPos, 500/4 * 2, Color.cyan);
+            }else{
+                mins[i].paint(g, xPos, 500/4 * 2, Color.black);
+            }
+            xPos += 50;
+        }
+        xPos = 500/5;
+        for(int i = 0; i < binarySeconds.length(); i++){
+            if(binarySeconds.substring(i, i+1).equals("1")){
+                secs[i].paint(g, xPos, 500/4 * 3, Color.cyan);
+            }else{
+                secs[i].paint(g, xPos, 500/4 * 3, Color.black);
+            }
+            xPos += 50;
+        }
 
-        if(binarySeconds.substring(0, 1).equals("1")){
-            secs[0].paint(g, 500/5, 500/4 * 3, Color.cyan);
-        }else{
-            secs[0].paint(g, 500/5, (500/4) * 3, Color.black);
-        }
-        if(binarySeconds.substring(1,2).equals("1")){
-            secs[1].paint(g, 500/5 + 50, 500/4 * 3, Color.cyan);
-        }else{
-            secs[1].paint(g, 500/5 + 50, (500/4) * 3, Color.black);
-        }
-        if(binarySeconds.substring(2,3).equals("1")){
-            secs[2].paint(g, 500/5 +100, 500/4 * 3, Color.cyan);
-        }else{
-            secs[2].paint(g, 500/5 + 100, (500/4) * 3, Color.black);
-        }
-        if(binarySeconds.substring(3,4).equals("1")){
-            secs[3].paint(g, 500/5 + 150, 500/4 * 3, Color.cyan);
-        }else{
-            secs[3].paint(g, 500/5 + 150, (500/4) * 3, Color.black);
-        }
-        if(binarySeconds.substring(4, 5).equals("1")){
-            secs[4].paint(g, 500/5 + 200, 500/4 * 3, Color.cyan);
-        }else{
-            secs[4].paint(g, 500/5 + 200, (500/4) * 3, Color.black);
-        }
-        if(binarySeconds.substring(5, 6).equals("1")){
-            secs[5].paint(g, 500/5 + 250, 500/4 * 3, Color.cyan);
-        }else{
-            secs[5].paint(g, 500/5 + 250, (500/4) * 3, Color.black);
-        }
+     
 
         repaint();
     }
